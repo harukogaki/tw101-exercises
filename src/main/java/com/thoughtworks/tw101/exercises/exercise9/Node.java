d56e647742ae29c1d637f3126498ba3a84707f18
Salvador Ariza <salvadorariza94@gmail.com>
2017-07-01 14:43:58 -0500

    Finished Exercise #9

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,60 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node prev;
+    private Node next;
 
-    public Node(String name) {
+    public Node(String name)
+    {
         this.name = name;
+        this.next = null;
+        this.prev = null;
     }
 
+    public String getName(){ return this.name; }
+
     public void add(String nameOfNewNode) {
+
+        //new node precedes this node
+        if (name.compareTo(nameOfNewNode) > 0){
+            if (this.prev != null)
+                this.prev.add(nameOfNewNode);
+            else
+                this.prev = new Node (nameOfNewNode);
+        }
+
+        //new node comes after this node
+        if (name.compareTo(nameOfNewNode) < 0){
+            if (this.next != null)
+                this.next.add(nameOfNewNode);
+            else
+                this.next = new Node (nameOfNewNode);
+        }
+
+
+    }
+
+    // recursive method that performs an in-order traversal of the root node
+    // inserting all the names into an ArrayList defined and passed by the caller
+    private void names_(List<String> names, Node root){
+        if (root.prev != null)
+            names_(names, root.prev);
+
+        names.add(root.getName());
+
+        if (root.next != null)
+            names_(names, root.next);
+
     }
 
     public List<String> names() {
-        return null;
+        List<String> allNames = new ArrayList<>();
+        names_(allNames, this);
+
+        return allNames;
     }
 }
