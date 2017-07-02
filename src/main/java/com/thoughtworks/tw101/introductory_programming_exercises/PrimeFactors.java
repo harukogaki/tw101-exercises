2581a262ed1311597c2d860b97fc8b8b9265738f
Salvador Ariza <salvadorariza94@gmail.com>
2017-07-02 18:18:49 -0500

    Finished Prime Factors

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,47 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+        int num = 30;
+        List<Integer> primeFactors = generate(num);
+
+        //Debugging purposes
+        System.out.println("Number: " + num);
+        System.out.println("Prime Factors: ");
+        for (Integer i : primeFactors){
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> primeFactors = new ArrayList<>();
+
+        //For all numbers, not including 1 and n
+        for(int i = 2; i < n; i++){
+
+            //If n is divisible by this number
+            if (n % i == 0) {
+
+                //but not a multiple of an already existing number
+                boolean notMultiple = true;
+
+                for (Integer integer : primeFactors){
+                    if (i % integer == 0) {
+                        notMultiple = false;
+                        break;
+                    }
+                }
+                //add as a prime factor
+                if (notMultiple)
+                    primeFactors.add(i);
+            }
+        }
+
+        return  primeFactors;
     }
 }
