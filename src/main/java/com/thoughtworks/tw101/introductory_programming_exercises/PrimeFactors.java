7208bbba9b4bbd9a9f8372bc4b04062a17512d0b
Salvador Ariza <salvadorariza94@gmail.com>
2017-07-11 13:44:15 -0500

    changes to Prime Factors... uncommented some code

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -15,12 +15,12 @@ public class PrimeFactors {
         List<Integer> primeFactors = generate(num);
 
         //For Debugging Purposes
-        //System.out.println("Number: " + num);
-        //System.out.println("Prime Factors: ");
-        //for (Integer i : primeFactors){
-            //System.out.print(i + " ");
-        //}
-        //System.out.println();
+        System.out.println("Number: " + num);
+        System.out.println("Prime Factors: ");
+        for (Integer i : primeFactors){
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
     private static List<Integer> generate(int n) {
